dec25a6fd0f9b3d32b869edfcaeec9377bc8afed
Felipe <luisfelipe7014feups@gmail.com>
2025-07-01 18:18:48 -0300

    Comecando tela de Cadastro e instalando react router dom

---
+++ src/screens/registerPage.tsx
@@ -0,0 +1,54 @@
+import logo from "../assets/logo/Logo.png";
+import onibus from "../assets/Onibus.png";
+import ButtonForm from "../components/buttonForm";
+import GoogleButton from "../components/googleButton";
+import InputEmail from "../components/inputEmail";
+import InputPassword from "../components/inputPassword";
+import LembrarButton from "../components/lembrarButton";
+import "./registerPage.css";
+
+function LoginPage() {
+  return (
+    <div id="container">
+      <aside id="left">
+        <img id="logoDirexa" src={logo} alt="Logo do sistema Direxa" />
+        <img id="busImg" src={onibus} alt="Imagem de um onibus na cidade" />
+      </aside>
+      <aside id="right">
+        <div id="rightButtons">
+          <ButtonForm
+            id="loginButton"
+            value="Login"
+            onClick={() => (window.location.href = "https://www.youtube.com/")}
+          />
+          <ButtonForm
+            id="registerButton"
+            value="Criar Conta"
+            onClick={() =>
+              (window.location.href = "https://drive.google.com/drive/u/0/home")
+            }
+          />
+        </div>
+        <form action="get">
+          <h2 id="titleLogin">Fazer Login</h2>
+          <InputEmail />
+          <InputPassword />
+          <div id="entrarAndLembrar">
+            <LembrarButton />
+            <ButtonForm
+              id="sendLogin"
+              value="Entrar"
+              onClick={() =>
+                (window.location.href =
+                  "https://drive.google.com/drive/u/0/home")
+              }
+            />
+          </div>
+          <GoogleButton />
+        </form>
+      </aside>
+    </div>
+  );
+}
+
+export default LoginPage;
