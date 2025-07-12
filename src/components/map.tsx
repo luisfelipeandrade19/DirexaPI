ef09adb000e56fc01e7b6f2f185be5642746b3b5
Luis Felipe <luisfelipe7014feups@gmail.com>
2025-07-12 13:44:18 -0300

    Adicionando mapa

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,26 +1,51 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css"; // Importe o CSS do Leaflet
+import "leaflet/dist/leaflet.css";
 import "./map.css";
+import { useEffect, useState } from "react";
 
 function Map() {
+  const [coords, setCoords] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+
+  useEffect(() => {
+    if (!navigator.geolocation) return;
+
+    const watchId = navigator.geolocation.watchPosition(
+      (position) => {
+        setCoords({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
+      (error) => {
+        console.error("Erro ao obter localização:", error);
+      }
+    );
+
+    return () => navigator.geolocation.clearWatch(watchId);
+  }, []);
+
   return (
     <div id="myMap">
-      <MapContainer 
-        center={[51.505, -0.09]} 
-        zoom={13} 
-        scrollWheelZoom={false}
-        style={{ height: "100%", width: "100%" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      {coords && (
+        <MapContainer
+          center={[coords.latitude, coords.longitude]}
+          zoom={16}
+          scrollWheelZoom={false}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            attribution=''
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+
+          <Marker position={[coords.latitude, coords.longitude]}>
+            <Popup>Você está aqui!</Popup>
+          </Marker>
+        </MapContainer>
+      )}
     </div>
   );
 }
